3cc05ff78e69e5aeb9291c67aa816d2b3298c67c
NathanThus <nathan.thus@outlook.com>
2025-08-18 20:42:24 +0200

    ✅ Add Full suite of tests including asynchronous

--- Assets/Scripts/Health/Tests/HealthTest.cs
+++ Assets/Scripts/Health/Tests/HealthTest.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using FoodWars.Health;
 using NUnit.Framework;
 using UnityEngine;
@@ -9,6 +11,7 @@ using UnityEngine.TestTools;
 public class HealthTest
 {
     private GameObject _testObject;
+    private float _defaultMaxHitpoints = 100;
 
     [SetUp]
     public void SetUp()
@@ -22,7 +25,8 @@ public class HealthTest
     [TearDown]
     public void TearDown()
     {
-        // Remove all components to ensure clean state for each test.
+        UnityEngine.Object.DestroyImmediate(_testObject.GetComponent<Health>());
+
         foreach (var comp in _testObject.GetComponents<Component>())
         {
             if (!(comp is Transform))
@@ -30,128 +34,352 @@ public class HealthTest
                 UnityEngine.Object.DestroyImmediate(comp);
             }
         }
+
     }
 
-    private Health AddHealthManager(float _maxHitPoints = 100f, float _hitPoints = 100f)
+    private Health SetupHealthComponent()
     {
-        var hm = _testObject.AddComponent<Health>();
-        // typeof(Health).GetField("_maxHitPoints", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-        //     .SetValue(hm, _maxHitPoints);
-        // typeof(Health).GetField("_hitPoints", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-        //     .SetValue(hm, _hitPoints);
+        _testObject.AddComponent<Rigidbody>();
+        _testObject.AddComponent<CapsuleCollider>();
+
+        var healthComponent = _testObject.AddComponent<Health>();
+
+        typeof(Health).GetField("_maxHitPoints", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+        .SetValue(healthComponent, _defaultMaxHitpoints);
 
         // Simulate Unity's Awake call
         var awake = typeof(Health).GetMethod("Awake", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-        awake.Invoke(hm, null);
+        awake.Invoke(healthComponent, null);
 
-        return hm;
+        return healthComponent;
     }
 
     [Test]
     public void Awake_DoesNotChangeIfAlreadyAtMax()
     {
-        var hm = AddHealthManager();
-        Assert.AreEqual(100, hm.HitPoints);
+        var healthObject = SetupHealthComponent();
+        Assert.AreEqual(healthObject.MaxHitPoints, healthObject.HitPoints);
     }
 
     [Test]
     public void DealDamage_ThrowsForNegativeInput()
     {
-        var hm = AddHealthManager();
-        Assert.Throws<ArgumentOutOfRangeException>(() => hm.DealDamage(-1f));
+        var healthObject = SetupHealthComponent();
+        Assert.Throws<ArgumentOutOfRangeException>(() => healthObject.DealDamage(-1f));
+    }
+
+    [Test]
+    public void DealDamage_DoesNotChangeIfZero()
+    {
+        var healthObject = SetupHealthComponent();
+        healthObject.DealDamage(0);
+        Assert.AreEqual(healthObject.MaxHitPoints, healthObject.HitPoints);
     }
 
     [Test]
     public void DealDamage_InvokesOnDamageForNonFatal()
     {
-        var hm = AddHealthManager(_maxHitPoints: 100f, _hitPoints: 100f);
+        var healthObject = SetupHealthComponent();
         bool damageCalled = false;
-        hm.OnDamage += () => damageCalled = true;
+        healthObject.OnDamage += () => damageCalled = true;
 
-        hm.DealDamage(10f);
+        float damage = 10f;
+        healthObject.DealDamage(damage);
 
         Assert.True(damageCalled);
-        Assert.AreEqual(90f, hm.HitPoints);
+        Assert.AreEqual(_defaultMaxHitpoints - damage, healthObject.HitPoints);
     }
 
     [Test]
     public void DealDamage_InvokesOnDeathWhenHitPointsZeroOrLess()
     {
-        var hm = AddHealthManager();
+        var healthObject = SetupHealthComponent();
         bool deathCalled = false;
-        hm.OnDeath += () => deathCalled = true;
+        healthObject.OnDeath += () => deathCalled = true;
 
-        hm.DealDamage(100f);
+        healthObject.DealDamage(_defaultMaxHitpoints);
 
         Assert.True(deathCalled);
-        Assert.AreEqual(0f, hm.HitPoints);
+        Assert.AreEqual(0, healthObject.HitPoints);
     }
 
     [Test]
     public void DealDamage_InvokesOnDeathWhenHitPointsGoesNegative()
     {
-        var hm = AddHealthManager();
+        var healthObject = SetupHealthComponent();
         bool deathCalled = false;
-        hm.OnDeath += () => deathCalled = true;
+        healthObject.OnDeath += () => deathCalled = true;
 
-        hm.DealDamage(101f);
+        healthObject.DealDamage(_defaultMaxHitpoints + 1);
 
         Assert.True(deathCalled);
-        Assert.AreEqual(-1f, hm.HitPoints);
+        Assert.AreEqual(-1f, healthObject.HitPoints);
     }
 
     [Test]
     public void Heal_ThrowsForNegativeInput()
     {
-        var hm = AddHealthManager();
-        Assert.Throws<ArgumentOutOfRangeException>(() => hm.Heal(-1f));
+        var healthObject = SetupHealthComponent();
+        Assert.Throws<ArgumentOutOfRangeException>(() => healthObject.Heal(-1f));
+    }
+
+
+    [Test]
+    public void Heal_DoesNotChangeIfZero()
+    {
+        var healthObject = SetupHealthComponent();
+        float damage = 20f;
+        healthObject.DealDamage(damage);
+        healthObject.Heal(0);
+        Assert.AreEqual(_defaultMaxHitpoints - damage, healthObject.HitPoints);
     }
 
     [Test]
     public void Heal_InvokesOnHeal()
     {
-        var hm = AddHealthManager();
+        var healthObject = SetupHealthComponent();
         bool healCalled = false;
-        hm.OnHeal += () => healCalled = true;
-        hm.DealDamage(10f);
-        hm.Heal(10f);
+        healthObject.OnHeal += () => healCalled = true;
+        healthObject.DealDamage(10f);
+        healthObject.Heal(10f);
 
         Assert.True(healCalled);
-        Assert.AreEqual(100f, hm.HitPoints);
+        Assert.AreEqual(_defaultMaxHitpoints, healthObject.HitPoints);
     }
 
     [Test]
     public void Heal_DoesNotExceedMaxHitPoints()
     {
-        var hm = AddHealthManager();
+        var healthObject = SetupHealthComponent();
 
-        hm.Heal(10f);
+        healthObject.Heal(10f);
 
-        Assert.AreEqual(100f, hm.HitPoints);
+        Assert.AreEqual(_defaultMaxHitpoints, healthObject.HitPoints);
     }
 
     [Test]
     public void Heal_AtMaxHitPoints_StaysAtMax()
     {
-        var hm = AddHealthManager();
+        var healthObject = SetupHealthComponent();
 
-        hm.Heal(1f);
+        healthObject.Heal(1f);
 
-        Assert.AreEqual(100f, hm.HitPoints);
+        Assert.AreEqual(_defaultMaxHitpoints, healthObject.HitPoints);
     }
 
     [Test]
     public void DealDamage_MultipleTimes_TriggersDeathOnce()
     {
-        var hm = AddHealthManager();
+        var healthObject = SetupHealthComponent();
         int deathCount = 0;
-        hm.OnDeath += () => deathCount++;
+        healthObject.OnDeath += () => deathCount++;
 
-        hm.DealDamage(90f); // Should not die
-        hm.DealDamage(10f); // Should die
-        hm.DealDamage(10f); // Should die again if called
+        healthObject.DealDamage(_defaultMaxHitpoints * 0.9f); // Alive
+        healthObject.DealDamage(_defaultMaxHitpoints * 0.1f); // Dead, with event
+        healthObject.DealDamage(_defaultMaxHitpoints * 0.1f); // Still dead, no event.
 
-        Assert.AreEqual(1, deathCount); // Because code allows OnDeath to be called on every call if <= 0
-        Assert.LessOrEqual(hm.HitPoints, 0f);
+        Assert.AreEqual(1, deathCount);
+        Assert.LessOrEqual(healthObject.HitPoints, 0f);
     }
+
+    [Test]
+    public async void Regeneration_WhenDamagedWithRegenerationEnabled_StartsAfterInitialDelay()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        bool regenerationStarted = false;
+        healthObject.OnRegenerationStarted += () => regenerationStarted = true;
+
+        healthObject.DealDamage(50f);
+
+        await UniTask.WaitForSeconds(4.5f);
+
+        Assert.IsTrue(regenerationStarted);
+        Assert.IsTrue(healthObject.IsRegenerating);
+    }
+
+    [Test]
+    public async void Regeneration_WhenDamagedWithRegenerationDisabled_DoesNotStart()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.DisableRegeneration();
+        bool regenerationStarted = false;
+        healthObject.OnRegenerationStarted += () => regenerationStarted = true;
+
+        healthObject.DealDamage(50f);
+
+        await UniTask.WaitForSeconds(5f);
+
+        // Assert
+        Assert.IsFalse(regenerationStarted);
+        Assert.IsFalse(healthObject.IsRegenerating);
+    }
+
+    [Test]
+    public async void Regeneration_WhenAtFullHealth_DoesNotStart()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        bool regenerationStarted = false;
+        healthObject.OnRegenerationStarted += () => regenerationStarted = true;
+
+        healthObject.DealDamage(0f);
+
+        await UniTask.WaitForSeconds(5f);
+
+        Assert.IsFalse(regenerationStarted);
+        Assert.IsFalse(healthObject.IsRegenerating);
+    }
+
+    [Test]
+    public async void Regeneration_HealsAtCorrectRate()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        float initialHealth = healthObject.HitPoints;
+
+        healthObject.DealDamage(30f);
+
+        await UniTask.WaitForSeconds(5.5f);
+
+
+        Assert.AreEqual(80f, healthObject.HitPoints, 0.1f);
+        Assert.IsTrue(healthObject.IsRegenerating);
+    }
+
+    [Test]
+    public async void Regeneration_ContinuesUntilFullHealth()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        bool regenerationStopped = false;
+        healthObject.OnRegenerationStopped += () => regenerationStopped = true;
+
+        healthObject.DealDamage(25f);
+        await UniTask.WaitForSeconds(16f);
+
+        Assert.AreEqual(100f, healthObject.HitPoints);
+        Assert.IsFalse(healthObject.IsRegenerating);
+        Assert.IsTrue(regenerationStopped);
+    }
+
+    [Test]
+    public async void Regeneration_MultipleHealTicks_InvokesOnHealEvent()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        int healEventCount = 0;
+        healthObject.OnHeal += () => healEventCount++;
+
+        healthObject.DealDamage(25f);
+
+        await UniTask.WaitForSeconds(15f);
+
+        Assert.AreEqual(3, healEventCount);
+    }
+
+    [Test]
+    public async void Regeneration_WhenDamagedDuringRegeneration_StopsAndRestarts()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        int regenerationStartedCount = 0;
+        int regenerationStoppedCount = 0;
+        healthObject.OnRegenerationStarted += () => regenerationStartedCount++;
+        healthObject.OnRegenerationStopped += () => regenerationStoppedCount++;
+
+        healthObject.DealDamage(50f);
+
+        await UniTask.WaitForSeconds(5f);
+
+        healthObject.DealDamage(10f); // Second damage during regeneration
+
+        await UniTask.WaitForSeconds(5f);
+
+        Assert.AreEqual(2, regenerationStartedCount); // Started twice
+        Assert.AreEqual(1, regenerationStoppedCount); // Stopped once (when interrupted)
+        Assert.IsTrue(healthObject.IsRegenerating); // Should be regenerating again
+    }
+
+    [Test]
+    public async void Regeneration_WhenHealedToFullExternally_Stops()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        bool regenerationStopped = false;
+        healthObject.OnRegenerationStopped += () => regenerationStopped = true;
+
+        healthObject.DealDamage(50f);
+
+        await UniTask.WaitForSeconds(5f);
+
+        healthObject.Heal(50f);
+
+        await UniTask.WaitForSeconds(1f);
+
+        Assert.IsFalse(healthObject.IsRegenerating);
+        Assert.IsTrue(regenerationStopped);
+    }
+
+    [Test]
+    public async void Regeneration_WhenKilled_DoesNotStart()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        bool regenerationStarted = false;
+        healthObject.OnRegenerationStarted += () => regenerationStarted = true;
+
+        healthObject.DealDamage(100f);
+
+        await UniTask.WaitForSeconds(5f);
+
+        Assert.IsTrue(healthObject.IsDead);
+        Assert.IsFalse(regenerationStarted);
+        Assert.IsFalse(healthObject.IsRegenerating);
+    }
+
+    [Test]
+    public async void Regeneration_WhenKilledDuringRegeneration_Stops()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        bool regenerationStopped = false;
+        healthObject.OnRegenerationStopped += () => regenerationStopped = true;
+
+        healthObject.DealDamage(50f);
+
+        await UniTask.WaitForSeconds(5f);
+
+        healthObject.DealDamage(60f);
+
+        await UniTask.WaitForSeconds(0.1f);
+
+        Assert.IsTrue(healthObject.IsDead);
+        Assert.IsFalse(healthObject.IsRegenerating);
+        Assert.IsTrue(regenerationStopped);
+    }
+
+    [Test]
+    public void Regeneration_WhenGameObjectDisabled_StopsGracefully()
+    {
+        var healthObject = SetupHealthComponent();
+
+        healthObject.EnableRegeneration();
+        healthObject.DealDamage(50f);
+
+        healthObject.gameObject.SetActive(false);
+
+        Assert.IsFalse(healthObject.IsRegenerating);
+    }
+
 }
